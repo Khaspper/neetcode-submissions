3f5ae9e85f86b93689ea44ca9434dab081674205
Mark Narciso <125713293+Khaspper@users.noreply.github.com>
2026-06-15 03:29:45 -0700

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums: number[], k: number): number {
+        let prefix: number = 0
+        const myMap = new Map<number, number>; 
+        myMap.set(0, 1)
+        let res: number = 0
+        for (const n of nums) {
+            prefix += n
+            const diff: number = prefix - k
+            if (myMap.has(diff)) {
+                res += myMap.get(diff)
+            }
+            myMap.set(prefix, (myMap.get(prefix) ?? 0) + 1)
+        }
+        console.log(myMap)
+        return res
+    }
+}
