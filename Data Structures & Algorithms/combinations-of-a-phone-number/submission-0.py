f0a7d46e6c9256f84b72d92447edb49f9ed5eb1f
Mark Narciso <125713293+Khaspper@users.noreply.github.com>
2026-07-18 14:45:14 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        myMap = {'2': ['a', 'b', 'c'], '3': ['d','e','f'], '4': ['g','h','i'],
+                 '5': ['j','k','l'], '6': ['m','n','o'], '7': ['p','q','r','s'], 
+                 '8': ['t','u','v'], '9':['w','x','y','z']}
+        res = []
+        def dfs(i, level, curr):
+            if len(curr) == len(digits):
+                res.append(''.join(curr))
+                return
+            if level >= len(digits) or i >= len(myMap[digits[level]]):
+                return
+            for j in range(len(myMap[digits[level]])):
+                curr.append(myMap[digits[level]][j])
+                dfs(j, level + 1, curr)
+                curr.pop()
+        dfs(0,0,[])
+        return res
